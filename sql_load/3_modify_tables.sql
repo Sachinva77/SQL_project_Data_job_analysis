6aa0e343c2860852cdc9ec09b85d5581ff11392a
Sachin <sachinva01@gmail.com>
2026-09-19 01:17:03 +0530

    upload sql files

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -29,42 +29,30 @@
  */
 -- NOTE: This has been updated from the video to fix issues with encoding
 COPY company_dim
-FROM 'C:\csv files\csv_files\company_dim.csv' WITH (
+FROM 'C:\SQL_project_Data_job_analysis\csv_files\company_dim.csv' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY skills_dim
-FROM 'C:\csv files\csv_files\skills_dim.csv' WITH (
+FROM 'C:\SQL_project_Data_job_analysis\csv_files\skills_dim.csv' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY job_postings_fact
-FROM 'C:\csv files\csv_files\job_postings_fact.csv' WITH (
+FROM 'C:\SQL_project_Data_job_analysis\csv_files\job_postings_fact.csv' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY skills_job_dim
-FROM 'C:\csv files\csv_files\skills_job_dim.csv' WITH (
+FROM 'C:\SQL_project_Data_job_analysis\csv_files\skills_job_dim.csv' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
-SELECT *
-FROM job_postings_fact
-LIMIT 100;
-SELECT *
-FROM company_dim
-LIMIT 100;
-SELECT *
-FROM skills_dim
-LIMIT 100;
-SELECT *
-FROM skills_job_dim
-LIMIT 100;
